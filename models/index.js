4060293727cde1a6ccc0d3e51605228eda88e3a3
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-09-14 15:24:39 -0700

    added models for Comment, Post, and User

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {
+    Post,
+    Commment
+}
