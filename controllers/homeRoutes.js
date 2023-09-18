4e58268223287df7b3b39e61dc790a16c8817c35
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-09-18 15:28:52 -0700

    added bootstrapped login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,4 +3,25 @@ const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
+
+//homepage
+
+
+//all posts page
+
+
+//one post page 
+
+
+
+//login page
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/')
+        return
+    }
+
+    res.render('login');
+})
+
 module.exports = router;
