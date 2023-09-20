5e3c0730562a1caa090e523920d151e93e37ce2d
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-09-20 09:28:47 -0700

    added signup logic - not working yet though..

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
+const { findByPk } = require('../models/Post');
 const withAuth = require('../utils/auth');
 
 
@@ -7,7 +8,7 @@ const withAuth = require('../utils/auth');
 //homepage
 router.get('/', async (req, res) => {
     try {
-        const postData = await Post.findAll({
+        const postsData = await Post.findAll({
             include: [
                 {
                     model: User,
@@ -16,7 +17,7 @@ router.get('/', async (req, res) => {
             ]
         });
 
-        const posts = postData.map((post) => post.get({ plain: true}));
+        const posts = postsData.map((post) => post.get({ plain: true}));
 
         res.render('homepage', {
             posts,
@@ -29,6 +30,27 @@ router.get('/', async (req, res) => {
 
 //all posts page
 //use the withAuth middle ware from 20 in the activities (already defined)
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'], 
+                },
+                {
+                    model: Comment,
+                    attributes: ['user_id', 'content', 'created_at']
+                }
+            ]
+        })
+        const post = postData.get({ plain: true });
+        console.log(post)
+    } 
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 //one post page 
 
--- public/js/login.js
+++ public/js/login.js
@@ -25,19 +25,18 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#signupUsername').value.trim();
+    const password = document.querySelector('#signupPassword').value.trim();
   
-    if (name && username && password) {
+    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, username, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
