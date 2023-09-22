0f6cd915da1c6eddbf662fb63bdd478dafbb1294
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-09-21 18:08:18 -0700

    removed email from user model to fix create user bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models')
 
 router.post('/', async (req, res) => {
   try {
+    console.log(req.body)
     const userData = await User.create(req.body);
+    console.log(userData);
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -13,6 +15,7 @@ router.post('/', async (req, res) => {
     })
 
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 })
